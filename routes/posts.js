0aae010c1e60bad6e23c33be10cce00c0a284e40
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2023-02-07 21:23:53 +0100

    fallos

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,56 @@
-var express = require('express');
+var express = require("express");
+var mongoose = require('mongoose');
 var router = express.Router();
-// middleware que es específico a este router
-router.use(function timeLog(req, res, next) {
-console.log('Fecha actual: ', Date.now());
-next();
+
+var Post = require('../models/Post');
+var User = require('../models/User');
+var db = mongoose.connection;
+
+
+// GET del listado de posts ordenados por fecha de publicación
+router.get("/", function (req, res, next) {
+  Post.find().sort("-publicationdate").populate("User").exec(function (err, posts) {
+      if (err) res.status(500).send(err);
+      else res.status(200).json(posts);
+    });
 });
-// define la ruta de la página del home
-router.get('/', function(req, res) {
-res.send('Página inicial de los posts');
+
+// GET de todos los posts de un usuario dado (identificado por su Id)
+router.get("/all/:id", function (req, res, next) {
+  Post.find({ user: req.params.id })
+    .sort("-publicationdate")
+    .populate("user")
+    .exec(function (err, posts) {
+      if (err) res.status(500).send(err);
+      else res.status(200).json(posts);
+    });
 });
-// define la ruta de la página about
-router.get('/about', function(req, res) {
-res.send('Acerca de los posts');
+
+// POST de un nuevo post o entrada
+router.post("/", function (req, res, next) {
+  User.findById(req.body.iduser, function (err, userinfo) {
+    if (err) res.status(500).send(err);
+    else {
+      // crear la instancia Post
+      var postInstance = new Post({
+        user: req.body.iduser,
+        title: req.body.title,
+        description: req.body.description,
+      });
+      // añadir postInstance al array de posts del usuario
+      userinfo.posts.push(postInstance);
+      // salvar el post en las colecciones users y posts
+      userinfo.save(function (err) {
+        if (err) res.status(500).send(err);
+        else {
+          postInstance.save(function (err) {
+            if (err) res.status(500).send(err);
+            res.sendStatus(200);
+          });
+        }
+      });
+    }
+  });
 });
+
 module.exports = router;
